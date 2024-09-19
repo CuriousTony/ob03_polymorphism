4c7be1f1e3e59ebcda1d0a32790fc2909ab568f2
CuriousTony <aszdorovtsev@yandex.ru>
2024-09-19 21:04:17 +0300

    Finished main part of the homework

--- main.py
+++ main.py
@@ -8,10 +8,9 @@ class Animal:
 
     def eat(self):
         pass
-    
-    
+
+
 class Bird(Animal):
-    # сократим кол-во писанины за счет **kwargs
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
@@ -25,7 +24,7 @@ class Mammal(Animal):
 
     def make_sound(self):
         print(f"{self.name.title()} издает леденящий кровь рык!")
-        
+
 
 class Reptile(Animal):
     def __init__(self, **kwargs):
@@ -36,36 +35,76 @@ class Reptile(Animal):
 
 
 def animal_sounds(animal):
-        animal.make_sound()
+    animal.make_sound()
 
 
-animals = [Bird(name="воробышек", kind='grey'),
-           Mammal(name="лев", kind='ferocious'),
-           Reptile(name="игуана", kind="skinny")]
+animals_dict = {
+    'bird': Bird(name="воробышек", kind='grey'),
+    'mammal': Mammal(name="лев", kind='ferocious'),
+    'reptile': Reptile(name="игуана", kind="skinny")
+}
 
-for animal in animals:
+for animal in animals_dict.values():
     animal_sounds(animal)
 
 
-# Используйте композицию для создания класса `Zoo`, который будет содержать
-# информацию о животных и сотрудниках. Должны быть методы для добавления
-# животных и сотрудников в зоопарк.
 class Employee:
-    def __init__(self, name, ability):
+    def __init__(self, name):
         self.name = name
-        self.ability = ability
 
-    def act(self):
-        pass
+
+class Zookeeper(Employee):
+    def __init__(self, spec, **kwargs):
+        super().__init__(**kwargs)
+        self.spec = spec
+
+    def feed_animals(self):
+        print(f"{self.name.title()} is feeding animals.")
+
+
+class Veterinarian(Employee):
+    def __init__(self, spec, **kwargs):
+        super().__init__(**kwargs)
+        self.spec = spec
+
+    def treat_animals(self):
+        print(f"{self.name.title()} is treating animals.")
 
 
 class Zoo:
-    def __init__(self, pets, employees):
-        self.pets = pets
-        self.employees = employees
+    def __init__(self):
+        self.employees = []
+        self.pets = []
 
-    def add_pet(self):
-        pass
+    # агрегация
+    def add_pet(self, pet_type):
+        if pet_type.lower() in animals_dict:
+            self.pets.append(animals_dict[pet_type.lower()])
+            print(f"{pet_type.title()} successfully transferred to the Zoo!")
 
-    def add_employee(self):
-        pass
+    # композиция
+    def add_employee(self, role):
+        if role == 'vet':
+            employee = Veterinarian(name='Mark', spec='veterinarian')
+            self.employees.append(employee)
+        elif role == 'keeper':
+            employee = Zookeeper(name='Tom', spec='zookeeper')
+            self.employees.append(employee)
+
+    def show_list(self, choice):
+        if choice == 'employee':
+            for guy in self.employees:
+                print(f"{guy.name.title()} the {guy.spec}")
+        elif choice == 'pets':
+            for pet in self.pets:
+                print(f"{pet.name} the {pet.kind}")
+
+
+leningradsky_zoopark = Zoo()
+leningradsky_zoopark.add_pet('bird')
+leningradsky_zoopark.add_pet('mammal')
+leningradsky_zoopark.add_pet('reptile')
+leningradsky_zoopark.show_list('pets')
+leningradsky_zoopark.add_employee('keeper')
+leningradsky_zoopark.add_employee('vet')
+leningradsky_zoopark.show_list('employee')
